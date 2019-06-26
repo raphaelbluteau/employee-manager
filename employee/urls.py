06be77e36a050655b1f6cd3188d38316faa3c623
Raphael Macedo de Carvalho <raphaelbluteau@gmail.com>
2019-06-26 01:36:35 -0300

    Adding employee models and endpoints

---
+++ employee/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('employee/', views.EmployeeList.as_view()),
+    re_path('employee/(?P<email>\w+|[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$',
+            views.EmployeeRetrieveDestroy.as_view()),
+]
