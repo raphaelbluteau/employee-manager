4998f230ae238b7b436bccbb801d5e3058d982e5
Raphael Macedo de Carvalho <raphaelbluteau@gmail.com>
2019-06-27 01:03:18 -0300

    Adding unit tests

--- employee/tests.py
+++ employee/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from employee.models import Employee
+from employee.views import EmployeeList
+
+list_view = EmployeeList.as_view()
+
+
+class EmployeeTest(TestCase):
+
+    def setUp(self):
+        User.objects.create_superuser(username='test_user', email='test_user@example.com', password='9pfgHk12')
+        Employee.objects.create(name="Lorem", email="lorem@ipsum.com", department="Dolor Department")
+
+    def test_employee_model_info(self):
+        """
+        Tests the creation of Employee model instances
+        """
+        employee = Employee.objects.get(email="lorem@ipsum.com")
+        self.assertEqual(employee.email, "lorem@ipsum.com")
+        self.assertEqual(employee.name, "Lorem")
+        self.assertEqual(employee.department, "Dolor Department")
+
+    def test_employee_endpoint_get(self):
+        """
+        Tests the endpoint /employee for the request method GET
+        """
+        self.client = Client()
+        self.client.login(username='test_user', password='9pfgHk12')
+        expected = [{'name': 'Lorem', 'email': 'lorem@ipsum.com', 'department': 'Dolor Department'}]
+
+        response = self.client.get(reverse('employee'), format='json')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(expected, response.data)
+
+    def test_employee_endpoint_post(self):
+        """
+        Tests the endpoint /employee for the request method POST
+        """
+        self.client = Client()
+        self.client.login(username='test_user', password='9pfgHk12')
+
+        response = self.client.post(reverse('employee'), {'name': 'Name', 'email': 'email@test.com',
+                                                          'department': 'D1'}, format='json')
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('D1', Employee.objects.get(email='email@test.com').department)
+        self.assertEqual('Name', Employee.objects.get(email='email@test.com').name)
+
+    def test_employee_endpoint_delete(self):
+        """
+        Tests the endpoint /employee/<pk> for the request method DELETE
+        """
+        self.client = Client()
+        self.client.login(username='test_user', password='9pfgHk12')
+
+        response = self.client.delete(reverse('employee_detail', args=['lorem@ipsum.com']), format='json')
+
+        self.assertEqual(204, response.status_code)
+        with self.assertRaises(Employee.DoesNotExist): Employee.objects.get(email='lorem@ipsum.com')
--- employee/urls.py
+++ employee/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('employee/', views.EmployeeList.as_view()),
+    path('employee/', views.EmployeeList.as_view(), name='employee'),
     re_path('employee/(?P<email>\w+|[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$',
-            views.EmployeeRetrieveDestroy.as_view()),
+            views.EmployeeRetrieveDestroy.as_view(), name='employee_detail'),
 ]
